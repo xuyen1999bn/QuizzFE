9b397643b4b425c89960832f01e2fd813930ce50
xuyen <xuyennhhgch17380@fpt.edu.vn>
2021-05-05 09:17:23 +0700

    s

--- client/src/configs/axiosInstance.js
+++ client/src/configs/axiosInstance.js
@@ -1,29 +1,31 @@
 import Axios from "axios";
 
 export const axiosInstance = Axios.create({
-    // baseURL: 'http://localhost:5000/api/v1'
-    baseURL: 'https://api-for-quizziz-game-v1.herokuapp.com/api/v1'
+  // baseURL: 'http://localhost:5000/api/v1'
+  baseURL: "https://api-for-quizziz-game-v1.herokuapp.com/api/v1",
 });
 
 axiosInstance.interceptors.request.use(function (config) {
-    const _localToken = localStorage.getItem('token_access');
-    if (_localToken)
-        config.headers.Authorization = "Bearer " + _localToken;
-    return config;
+  const _localToken = localStorage.getItem("token_access");
+  if (_localToken) config.headers.Authorization = "Bearer " + _localToken;
+  return config;
 });
 
-axiosInstance.interceptors.response.use(function (response) {
+axiosInstance.interceptors.response.use(
+  function (response) {
     return response;
-}, function (error) {
+  },
+  function (error) {
     const resError = error.response;
     if (resError?.status === 401) {
-        localStorage.removeItem('token_access');
-        window.location.href = '/auth/login';
+      localStorage.removeItem("token_access");
+      window.location.href = "/auth/login";
     }
     return Promise.reject(error);
-});
+  }
+);
 
 export const axiosInstanceAuth = Axios.create({
-    // baseURL: 'http://localhost:5000/api/v1'
-    baseURL: 'https://api-for-quizziz-game-v1.herokuapp.com/api/v1'
+  // baseURL: 'http://localhost:5000/api/v1'
+  baseURL: "https://api-for-quizziz-game-v1.herokuapp.com/api/v1",
 });
